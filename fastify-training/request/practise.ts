758620d6f1f78d271e5ad45b742cb79446498aa0
akshay-cloud-dev <sacred.socialmedia0@gmail.com>
2026-05-23 23:31:21 +0530

    feat: add additional routes for /listen and /akshay/:id with logging and request details

--- fastify-training/request/practise.ts
+++ fastify-training/request/practise.ts
@@ -33,6 +33,46 @@ fastify.get('/headers', async(request, reply)=>{
         headers: request.headers,
     }
 })
+
+fastify.get('/listen', async(request, reply)=>{
+    request.log.info('Info log');
+    request.log.warn('Warning log');
+    request.log.info({
+            user: "akshay",
+            role: "admin",
+            });
+
+    return {
+        requestId: request.id,
+    }
+})
+
+fastify.get('/akshay/:id', async(request, reply)=>{
+  console.log("\n=== request.id ===");
+  console.log(request.id);
+
+  console.log("\n=== request.server ===");
+  console.log(request.server);
+
+  console.log("\n=== request.routeOptions ===");
+  console.log(request.routeOptions);
+
+  console.log("\n=== request.raw ===");
+  console.log(request.raw);
+
+  return {
+    requestId: request.id,
+
+    route: {
+      method: request.routeOptions.method,
+      url: request.routeOptions.url,
+      logLevel: request.routeOptions.logLevel,
+      bodyLimit: request.routeOptions.bodyLimit,
+    },
+
+  }
+})
+
  
 fastify.listen({port: 3000}, (err, address)=>{
     if(err){  
